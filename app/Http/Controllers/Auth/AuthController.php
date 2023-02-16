87c00d1adba9a09f25d40ca77196ec7d181067ca
Gary HL <103480668+GaryHL@users.noreply.github.com>
2023-02-16 21:11:04 +0100

    created test register admin

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -68,7 +68,7 @@ class AuthController extends Controller
         return response()->json([
             'user' => $user,
             'token' => $token
-        ], 201);
+        ], 200);
     }
 
 }
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,18 +2,20 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
 use Tymon\JWTAuth\Facades\JWTAuth;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class UserTest extends TestCase
 {
     public function setUp(): void
     {
         parent::setUp();
-        Artisan::call('migrate');
+        Artisan::call('migrate --seed');
     }
 
     public function tearDown(): void
@@ -74,6 +76,45 @@ class UserTest extends TestCase
         $response->assertStatus(200);
 
         $response->assertJsonStructure(['token']);
+        
+    }
+
+    public function test_register_admin_return_access_token()
+    {
+
+        $dataUser = [
+            'name' => 'nameexample',
+            'email' => 'example@email.com',
+            'password' => 'examplepassword',
+            'password_confirmation' => 'examplepassword',
+        ];
+
+        $data_super_admin = [
+            'email' => 'super_admin@gmail.com',
+            'password' => 'adminpassword',
+        ];
+        
+        $response = $this->post('/api/login',$data_super_admin);
+        
+        $response->assertStatus(200);
+        
+
+        $token = $response['token'];
+
+
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer ' . $token,
+        ])->post('/api/register_admin', $dataUser);
+        
+        $response->assertStatus(200);
+        
+        $response->assertJsonStructure(['token']);
+        
+        $this->assertDatabaseHas('users', [
+            'name' => 'nameexample',
+            'email' => 'example@email.com',
+            'role' => 'admin',
+        ]);
         
     }
 }
